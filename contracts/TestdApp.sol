d080b8c25bf7d7545b60dcdc900ee3c5705522f3
Ephraim O. Oladepo <theflamepen@gmail.com>
2022-06-08 17:09:29 +0100

    Frontend updated.

--- contracts/TestdApp.sol
+++ contracts/TestdApp.sol
@@ -2,17 +2,17 @@
 pragma solidity ^0.8.0;
 
 contract TestdApp {
-    string public appName;
-    string public appClass;
+    string public  constant appName = "Test dApp";
+    string public appClass = "Decentralized App";
 
 
     function setClass(string memory _appClass) public view {
         _appClass = appClass;
     }
 
-    function setName(string memory _appName) public view {
-        _appName = appName;
-    }
+    // function setName(string memory _appName) public view {
+    //     _appName = appName;
+    // }
 
 
     function getName() public view returns (string memory) {
